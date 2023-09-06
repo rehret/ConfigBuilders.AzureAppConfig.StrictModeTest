5c3099f2fcb73f9e2fd3dd0616a480f6264579df
Randy Ehret <11415001+rehret@users.noreply.github.com>
2023-09-06 14:02:23 -0500

    Create sample project of AAC Strict-mode issue

---
+++ ConfigBuilders.AzureAppConfig.StrictModeTest/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConfigBuilders.AzureAppConfig.StrictModeTest
+{
+    using System;
+    using System.Configuration;
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"TestKey (Env): '{ConfigurationManager.AppSettings["TestKey:Env"]}'");
+            Console.WriteLine($"TestKey (AAC): '{ConfigurationManager.AppSettings["TestKey:AAC"]}'");
+        }
+    }
+}
